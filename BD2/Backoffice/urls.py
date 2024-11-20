5c25188a55bd043707f69aa39ffceb1fb9687679
Diogo3ms <96895102+Diogo3ms@users.noreply.github.com>
2024-11-20 11:31:56 +0000

    alteração url pedidos

--- BD2/Backoffice/urls.py
+++ BD2/Backoffice/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('contracts/',  views.contracts, name='contracts'),
     path('contractdetail/',  views.contractdetail, name='contractdetail'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('pedidos/', views.pedidos, name='orders'),
-    path('pedidosdetail/', views.pedidos_detail, name='pedidosdetail'),
+    path('request/', views.pedidos, name='orders'),
+    path('requestdetail/', views.pedidos_detail, name='pedidosdetail'),
 
 
 ]
